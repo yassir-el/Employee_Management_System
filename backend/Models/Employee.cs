bcbe8610ea6c6278440a6ca083aeaef838711202
yassir-el <elyassirzagora@gmail.com>
2024-07-09 11:13:00 +0100

    Update Employee model with Required properties and check if Email is valid

--- backend/Models/Employee.cs
+++ backend/Models/Employee.cs
@@ -1,15 +1,26 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace EmployeeManagementSystem.Models
 {
+using System;
+
     public class Employee
     {
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Position { get; set; }
         public string Department { get; set; }
     }
+
 }

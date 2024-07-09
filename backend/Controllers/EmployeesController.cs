ea87799a759a126f4f8acb7613030b9ee8f71ecc
yassir-el <elyassirzagora@gmail.com>
2024-07-09 13:27:21 +0100

    update EmployeeController with CRUD endpoints

--- backend/Controllers/EmployeesController.cs
+++ backend/Controllers/EmployeesController.cs
@@ -31,5 +31,48 @@ namespace EmployeeManagementSystem.Controllers
 
             return remployee;
         }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployee(Guid id, Employee employee)
+        {
+            employee.Id = id;
+            _context.Entry(employee).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(Guid id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EmployeeExists(Guid id)
+        {
+            return _context.Employees.Any(e => e.Id.Equals(id));
+        }
     }
 }
